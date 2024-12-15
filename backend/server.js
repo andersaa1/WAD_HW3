429fecfe2f8c86b628959f21df5a581a1863b047
andersaa1 <ander.saarniit@gmail.com>
2024-12-15 21:03:20 +0200

    log in backend logic

--- backend/server.js
+++ backend/server.js
@@ -85,3 +85,26 @@ app.post('signup', async(req, res) => {
         res.status(400).send(err.message);
     }
 });
+
+app.post('/auth/login', async(req, res) => {
+    try {
+        console.log("a login request has arrived");
+        const { email, password } = req.body;
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (user.rows.length === 0) return res.status(401).json({ error: "User is not registered" });
+
+        //Checking if the password is correct
+        const validPassword = await bcrypt.compare(password, user.rows[0].password);
+        //console.log("validPassword:" + validPassword);
+        if (!validPassword) return res.status(401).json({ error: "Incorrect password" });
+
+        const token = await generateJWT(user.rows[0].id);
+        res
+            .status(201)
+            .cookie('jwt', token, { maxAge: 6000000, httpOnly: true })
+            .json({ user_id: user.rows[0].id })
+            .send;
+    } catch (error) {
+        res.status(401).json({ error: error.message });
+    }
+});
